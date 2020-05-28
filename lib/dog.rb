af0bc8cd577f26f513195d75f5f507750f0f2d4e
Wenhao Guo <gwh1742072653@gmail.com>
2020-05-28 20:53:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+fido = dog.new("fido")
+
+class Dog 
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.each do |name|
+      puts name
+    end
+  end
+end
+
+
+
